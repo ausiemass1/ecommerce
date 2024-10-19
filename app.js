5eb4b455135b9bb9cb4a6c1ffb327ed530870ac1
ausiemass1 <ausiemass@gmail.com>
2024-10-20 01:33:12 +1300

    added code to send session data to all other pages

--- app.js
+++ app.js
@@ -49,11 +49,12 @@ app.get("/register", (req, res) => res.render("register"));
 app.get("/contact", (req, res) => res.render("contact"));
 app.get("/products", (req, res) => res.render("products"));
 app.get("/admin", (req, res) => {
-  conn.query("SELECT * FROM cart", (err, results)=>{
-    if(err) throw err;
-    res.render("adminviews/", {results: results});
-  })
-  });
+  // conn.query("SELECT * FROM cart", (err, results)=>{
+  //   if(err) throw err;
+    res.render("adminviews/");
+  }
+)
+//   });
 app.get("/admin/insertbrand", (req, res) =>
   res.render("adminviews/insertbrand")
 );
@@ -95,7 +96,7 @@ app.get("/admin/users", (req, res) => {
         if (match) {
           // Set user session and redirect to the dashboard
           req.session.loggedin = true;
-          req.session.user = user;
+          req.session.username = username;
           req.flash('success_msg', ' Successfuly logged in!');
           res.redirect('/admin');
         } else {
